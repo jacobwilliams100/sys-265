f1af67db5d79f894f3f7c2d4c8bac0e0def75699
jacobwilliams100 <jacob.williams@mymail.champlain.edu>
2024-02-28 20:13:32 +0000

    wrote secure-ssh.sh script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,3 +4,21 @@
 #adds a public key fro the local repo or curled from the remote repo
 #removes roots ability to ssh in
 echo "ALL YOUR CODE GOES HERE"
+
+username=$1
+
+useradd -m -d /home/$username  -s /bin/bash $username
+
+mkdir /home/$username/.ssh
+
+#mkdir /home/$username/.ssh/authorized_keys
+
+cp /home/jacob/SYS-265/public-keys/id_rsa.pub /home/$username/.ssh/authorized_keys
+
+chmod 700 /home/$username/.ssh
+
+chmod 600 /home/$username/.ssh/authorized_keys
+
+chown -R $username:$username /home/$username/.ssh
+
+echo "DONE"
